a875791bce877daa22a302015e6d8c8173e48886
ekkozzz <g1004310045@gmail.com>
2021-06-04 01:27:59 +0800

    feat：基本完成

--- src/http.js
+++ src/http.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 const http = axios.create({
   // baseURL: 'https://www.liulongbin.top:8888/api/private/v1/',
   baseURL: 'http://timemeetyou.com:8889/api/private/v1/',
@@ -6,11 +8,20 @@ const http = axios.create({
 
 http.interceptors.request.use(
   (config) => {
+    NProgress.start()
     config.headers.Authorization = window.sessionStorage.getItem('token')
     return config
   },
   (error) => {
-    // 对请求错误做些什么
+    return Promise.reject(error)
+  }
+)
+http.interceptors.response.use(
+  (config) => {
+    NProgress.done()
+    return config
+  },
+  (error) => {
     return Promise.reject(error)
   }
 )
